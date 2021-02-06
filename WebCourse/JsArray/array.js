2357340a57d7d0ca87607dec7e459eee9666b78f
Vlad <vladislav0000054@gmail.com>
2021-02-06 22:25:23 +0700

    Правки по проекту

--- WebCourse/JsArray/array.js
+++ WebCourse/JsArray/array.js
@@ -1,54 +1,67 @@
 ﻿(function () {
     var numbers = [5, 9, 7, 1, -5, 0, 88, 325, -40, 79, 66, -73, 5];
 
-    function sortArray(array) {
-        array.sort((a, b) => a - b);
-        console.log("Отсортированный массив: ", numbers);
+    function sortArray() {
+        numbers.sort(function (a, b) {
+            return b - a;
+        });
     }
 
-    function printFirst5Items(array) {
-        var first5Items = array.slice(0, 5);
-        console.log("Первые пять элементов массива: ", first5Items);
+    function getFirst5Items() {
+        return numbers.slice(0, 5);
     }
 
-    function printLast5Items(array) {
-        var last5Items = array.slice(numbers.length - 5);
-        console.log("Последние пять элементов массива: ", last5Items);
+    function getLast5Items() {
+        return numbers.slice(numbers.length - 5);
     }
 
-    function printEvenNumbersSum(array) {
-        var evenNumbersSum = array.filter(x => x % 2 === 0).reduce((sum, item) => sum + item);
-        console.log("Сумма четных чисел массива: ", evenNumbersSum);
+    function getEvenNumbersSum() {
+        return numbers
+            .filter(function (item) {
+                return item % 2 === 0;
+            })
+            .reduce(function (sum, item) {
+                return sum + item;
+            }, 0);
     }
 
-    sortArray(numbers);
-    printFirst5Items(numbers);
-    printLast5Items(numbers);
-    printEvenNumbersSum(numbers);
+    sortArray();
+    console.log("Отсортированный массив по убыванию: ", numbers);
+
+    var first5Items = getFirst5Items();
+    console.log("Первые пять элементов массива: ", first5Items);
+
+    var last5Items = getLast5Items();
+    console.log("Последние пять элементов массива: ", last5Items);
+
+    var evenNumbersSum = getEvenNumbersSum();
+    console.log("Сумма четных чисел массива: ", evenNumbersSum);
 })();
-
 
 (function () {
-    var baseArray;
-
     function createArray() {
-        baseArray = new Array(100);
+        var baseArray = [];
 
-        for (var i = 0; i < baseArray.length; i++) {
-            baseArray[i] = i + 1;
+        for (var i = 0; i < 100; i++) {
+            baseArray.push(i + 1);
         }
 
-        console.log("Массив из 100 чисел: ", baseArray);
+        return baseArray;
     }
 
-    function getEvenSquaresNumbers(array) {
-        return array.filter(item => item % 2 === 0).map(item => Math.pow(item, 2));
+    var array = createArray();
+    console.log("Массив из 100 чисел: ", array);
+
+    function getEvenSquaresNumbers() {
+        return array
+            .filter(function (item) {
+                return item % 2 === 0;
+            })
+            .map(function (item) {
+                return Math.pow(item, 2);
+            });
     }
 
-    createArray();
-
-    var evenSquaresNumbers = getEvenSquaresNumbers(baseArray);
+    var evenSquaresNumbers = getEvenSquaresNumbers();
     console.log("Массив квадратов четных чисел: ", evenSquaresNumbers);
 })();
-
-

ba8ba2684fdedf5d6047199be9d39e0f7638a1ba
Vlad <vladislav0000054@gmail.com>
2021-02-15 15:50:11 +0700

    Правки по проекту

--- WebCourse/JsArray/array.js
+++ WebCourse/JsArray/array.js
@@ -1,22 +1,22 @@
 ﻿(function () {
     var numbers = [5, 9, 7, 1, -5, 0, 88, 325, -40, 79, 66, -73, 5];
 
-    function sortArray() {
-        numbers.sort(function (a, b) {
+    function sortArray(array) {
+        array.sort(function (a, b) {
             return b - a;
         });
     }
 
-    function getFirst5Items() {
-        return numbers.slice(0, 5);
+    function getFirst5Items(array) {
+        return array.slice(0, 5);
     }
 
-    function getLast5Items() {
-        return numbers.slice(numbers.length - 5);
+    function getLast5Items(array) {
+        return array.slice(numbers.length - 5);
     }
 
-    function getEvenNumbersSum() {
-        return numbers
+    function getEvenNumbersSum(array) {
+        return array
             .filter(function (item) {
                 return item % 2 === 0;
             })
@@ -25,16 +25,16 @@
             }, 0);
     }
 
-    sortArray();
+    sortArray(numbers);
     console.log("Отсортированный массив по убыванию: ", numbers);
 
-    var first5Items = getFirst5Items();
+    var first5Items = getFirst5Items(numbers);
     console.log("Первые пять элементов массива: ", first5Items);
 
-    var last5Items = getLast5Items();
+    var last5Items = getLast5Items(numbers);
     console.log("Последние пять элементов массива: ", last5Items);
 
-    var evenNumbersSum = getEvenNumbersSum();
+    var evenNumbersSum = getEvenNumbersSum(numbers);
     console.log("Сумма четных чисел массива: ", evenNumbersSum);
 })();
 
@@ -42,17 +42,17 @@
     function createArray() {
         var baseArray = [];
 
-        for (var i = 0; i < 100; i++) {
-            baseArray.push(i + 1);
+        for (var i = 1; i <= 100; i++) {
+            baseArray.push(i);
         }
 
         return baseArray;
     }
 
-    var array = createArray();
-    console.log("Массив из 100 чисел: ", array);
+    var numbersArray = createArray();
+    console.log("Массив из 100 чисел: ", numbersArray);
 
-    function getEvenSquaresNumbers() {
+    function getSquaresEvenNumbers(array) {
         return array
             .filter(function (item) {
                 return item % 2 === 0;
@@ -62,6 +62,6 @@
             });
     }
 
-    var evenSquaresNumbers = getEvenSquaresNumbers();
-    console.log("Массив квадратов четных чисел: ", evenSquaresNumbers);
+    var squaresEvenNumbers = getSquaresEvenNumbers(numbersArray);
+    console.log("Массив квадратов четных чисел: ", squaresEvenNumbers);
 })();

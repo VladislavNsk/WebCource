fa87ceca5e138624ff074e04b7c16ef1238edaf4
Vlad <vladislav0000054@gmail.com>
2021-03-08 17:49:43 +0700

    Правки по проекту

--- WebCourse/TemperatureConverter/Scripts/Script.js
+++ WebCourse/TemperatureConverter/Scripts/Script.js
@@ -2,29 +2,29 @@
 
 var TemperatureConverter = TemperatureConverter || {};
 
-TemperatureConverter.ConvertToFarengeit = function (celsius) {
-    return (celsius * 9 / 5) + 32;
+TemperatureConverter.convertToFahrenheit = function (celsius) {
+    return ((celsius * 9 / 5) + 32).toFixed(1);
 };
 
-TemperatureConverter.ConvertToKelvin = function (celsius) {
-    return celsius + 273.15;
+TemperatureConverter.convertToKelvin = function (celsius) {
+    return (celsius + 273.15).toFixed(1);
 };
 
-TemperatureConverter.Celsius = document.getElementById("celcius");
-TemperatureConverter.Kelvin = document.getElementById("kelvin");
-TemperatureConverter.Farengeit = document.getElementById("farengeit");
+TemperatureConverter.celsius = document.getElementById("celsius");
+TemperatureConverter.kelvin = document.getElementById("kelvin");
+TemperatureConverter.fahrenheit = document.getElementById("fahrenheit");
 
-TemperatureConverter.PrintError = function (message) {
-    this.ClearError();
+TemperatureConverter.printError = function (message) {
+    this.clearError();
 
     var messageElement = document.createElement("div");
     messageElement.textContent = message;
-    messageElement.style.width = TemperatureConverter.Celsius.offsetWidth + "px";
+    messageElement.style.width = TemperatureConverter.celsius.offsetWidth + "px";
     messageElement.classList.add("error");
-    TemperatureConverter.Celsius.parentElement.appendChild(messageElement);
+    TemperatureConverter.celsius.parentElement.appendChild(messageElement);
 };
 
-TemperatureConverter.ClearError = function() {
+TemperatureConverter.clearError = function() {
     var errors = document.getElementsByClassName("error");
 
     if (errors.length !== 0) {
@@ -34,27 +34,21 @@ TemperatureConverter.ClearError = function() {
 
 (function () {
     function convert() {
-        if (isNaN(TemperatureConverter.Celsius.value)) {
-            TemperatureConverter.PrintError("Введены не корректные значения");
+        if (isNaN(TemperatureConverter.celsius.value)) {
+            TemperatureConverter.printError("Введены не корректные значения");
 
             return;
         }
 
-        if (TemperatureConverter.Celsius.value.length === 0) {
-            TemperatureConverter.PrintError("Введите градусы");
-
+        if (TemperatureConverter.celsius.value.length === 0) {
+            TemperatureConverter.printError("Введите градусы");
             return;
         }
 
-        TemperatureConverter.ClearError();
-        TemperatureConverter.Kelvin.value = TemperatureConverter.ConvertToKelvin(+TemperatureConverter.Celsius.value);
-        TemperatureConverter.Farengeit.value = TemperatureConverter.ConvertToFarengeit(+TemperatureConverter.Celsius.value);
+        TemperatureConverter.clearError();
+        TemperatureConverter.kelvin.value = TemperatureConverter.convertToKelvin(+TemperatureConverter.celsius.value);
+        TemperatureConverter.fahrenheit.value = TemperatureConverter.convertToFahrenheit(+TemperatureConverter.celsius.value);
     }
 
     document.getElementById("button").addEventListener("click", convert);
 })();
-
-
-
-
-
